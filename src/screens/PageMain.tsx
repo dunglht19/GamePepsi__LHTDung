759735f29ed3e3156f59c750b4e7d9b53a25fecd
dunglht19 <dunglht19@gmail.com>
2022-01-27 09:41:46 +0700

    fix 03

--- src/screens/PageMain.tsx
+++ src/screens/PageMain.tsx
@@ -12,128 +12,199 @@ import {
   Modal,
   Pressable,
 } from "react-native";
-import { Icon } from '@iconify/react';
-import { Feather } from '@expo/vector-icons';
-
-
+import { Icon } from "@iconify/react";
+import { Feather } from "@expo/vector-icons";
+import { AntDesign } from '@expo/vector-icons';
 
 const PageMain = (props) => {
-     const { navigation } = props;
-     const [modalVisible, setModalVisible] = useState(false);
-     const createTwoButtonAlert = () =>
-    Alert.alert('Alert Title', 'My Alert Msg', [
-      {
-        text: 'Cancel',
-        onPress: () => console.log('Cancel Pressed'),
-        style: 'cancel',
-      },
-      { text: 'OK', onPress: () => console.log('OK Pressed') },
-    ]);
+  const { navigation } = props;
+  const [modalVisible, setModalVisible] = useState(false);
+  const [modalVisible2, setModalVisible2] = useState(false);
 
-    const onSignIn=()=>{
-            navigation.navigate('SignIn');
-        }
-          
-      
+  const onSignIn = () => {
+    navigation.navigate("SignIn");
+  };
 
   //  const onSignIn=()=>{
   //           navigation.navigate('SignIn');
   //       }
-    return (
-       <View style={styles.container}>
+  return (
+    <View style={styles.container}>
       <ImageBackground
         source={require("../../assets/bg2.png")}
         resizeMode="cover"
         style={styles.image}
       >
-      <Modal
-        animationType="slide"
-        transparent={true}
-        visible={modalVisible}
-        onRequestClose={() => {
-          Alert.alert('Modal has been closed.');
-          setModalVisible(!modalVisible);
-        }}>
-        <View style={styles.centeredView}>
-          <View style={styles.modalView}>
-            <Text style={styles.modalText}>Bạn có chắc muốn <Text style={{fontWeight:'bold'}}>đăng xuất</Text> không?</Text>
-           
-            <Pressable
-              style={[styles.button]}
-              onPress={onSignIn}>
-              <Text style={styles.textStyle}>Đăng xuất</Text>
-            </Pressable>
+        <Modal
+          animationType="slide"
+          transparent={true}
+          visible={modalVisible}
+          onRequestClose={() => {
+            Alert.alert("Modal has been closed.");
+            setModalVisible(!modalVisible);
+          }}
+        >
+          <View style={styles.centeredView}>
+            <View style={styles.modalView}>
+              <Text style={styles.modalText}>
+                Bạn có chắc muốn{" "}
+                <Text style={{ fontWeight: "bold" }}>đăng xuất</Text> không?
+              </Text>
 
-            <TouchableOpacity
-             
-              onPress={() => setModalVisible(!modalVisible)}>
-              <ImageBackground
-              source={require("../../assets/white_button1.png")}
-              resizeMode="cover" style={{justifyContent: "center",
-    width: 130,
-    height:50}}
-              >           
-              <Text style={{color:'#0063A7',fontWeight:'bold',textAlign:'center',marginBottom:7}}>Hủy</Text>
-            </ImageBackground>              
-            </TouchableOpacity>            
+              <Pressable style={[styles.button]} onPress={onSignIn}>
+                <Text style={styles.textStyle}>Đăng xuất</Text>
+              </Pressable>
+
+              <TouchableOpacity onPress={() => setModalVisible(!modalVisible)}>
+                <ImageBackground
+                  source={require("../../assets/white_button1.png")}
+                  resizeMode="cover"
+                  style={{ justifyContent: "center", width: 130, height: 50 }}
+                >
+                  <Text
+                    style={{
+                      color: "#0063A7",
+                      fontWeight: "bold",
+                      textAlign: "center",
+                      marginBottom: 7,
+                    }}
+                  >
+                    Hủy
+                  </Text>
+                </ImageBackground>
+              </TouchableOpacity>
+            </View>
           </View>
-        </View>
-      </Modal> 
-      <Feather style={{position:"absolute",top:30,right:30}} name="log-out" size={20} color="white" onPress={() => setModalVisible(true)}/>      
+        </Modal>
+        <Feather
+          style={{ position: "absolute", top: 30, right: 30 }}
+          name="log-out"
+          size={21}
+          color="white"
+          onPress={() => setModalVisible(true)}
+        />
         <View style={styles.top}>
-       
-         {/* <Icon style={{position:"absolute",right:10,top:-40,}} icon="mono-icons:log-out"  width="20" height="20" color="white" inline={true} />          */}
+          {/* <Icon style={{position:"absolute",right:10,top:-40,}} icon="mono-icons:log-out"  width="20" height="20" color="white" inline={true} />          */}
 
-         
-         <Image style={styles.img}   source={require("../../assets/daulan.png")}/>         
-         
+          <Image
+            style={styles.img}
+            source={require("../../assets/daulan.png")}
+          />
         </View>
 
         <View style={styles.bottom}>
-        <Text onPress={createTwoButtonAlert} style={{color:'#FFDD00',fontSize:17,fontWeight:'bold',marginBottom:5,textAlign:"center"}}>Hướng Dẫn</Text>
-        <TouchableOpacity style={styles.touchable}>
+          <Text
+            style={{
+              color: "#FFDD00",
+              fontSize: 17,
+              fontWeight: "bold",
+              marginBottom: 5,
+              textAlign: "center",
+            }}
+          >
+            Hướng Dẫn
+          </Text>
+
+          <Modal
+            animationType="slide"
+            transparent={true}
+            visible={modalVisible2}
+            onRequestClose={() => {
+              Alert.alert("Modal has been closed.");
+              setModalVisible2(!modalVisible2);
+            }}
+          >
+            <View style={styles.centeredView2}>
+              <View style={styles.modalView2}>
+                <AntDesign name="closecircle" size={24} color="#D02027" style={{ position: "relative",left:100,bottom:5 }} onPress={() => setModalVisible2(!modalVisible2)}/>
+                <Text style={styles.modalText2}>
+                  BẠN CÓ MUỐN SỬ DỤNG LƯỢT CHƠI NÀO?
+                </Text>
+
+                <TouchableOpacity >                  
+                  <ImageBackground
+                    source={require("../../assets/red_button1.png")}
+                    resizeMode="cover"
+                    style={{ justifyContent: "center", width: 200, height: 60 }}
+                  >
+                    <Text style={styles.title_Modal}>Lượt chơi miễn phí</Text>
+                    <Text style={styles.intitle_Modal}>
+                      Bạn còn <Text style={{ color: "yellow" }}>0</Text> lượt
+                      chơi
+                    </Text>
+                  </ImageBackground>
+                </TouchableOpacity>
+
+                <TouchableOpacity>                                  
+                  <ImageBackground
+                    source={require("../../assets/red_button1.png")}
+                    resizeMode="cover"
+                    style={{ justifyContent: "center", width: 200, height: 60 }}
+                  >
+                    <Text style={styles.title_Modal}>Lượt chơi quy đổi</Text>
+                    <Text style={styles.intitle_Modal}>
+                      Bạn còn <Text style={{ color: "yellow" }}>0</Text> lượt
+                      chơi
+                    </Text>
+                  </ImageBackground>
+                </TouchableOpacity>
+              </View>
+            </View>
+          </Modal>
+
+          <TouchableOpacity
+            style={styles.touchable}
+            onPress={() => setModalVisible2(true)}
+          >
             <ImageBackground
-               source={require("../../assets/button3.png")}
+              source={require("../../assets/button3.png")}
               resizeMode="cover"
-              style={styles.imageButton1}>            
+              style={styles.imageButton1}
+            >
               <Text style={styles.title}>Chơi ngay</Text>
-              <Text style={styles.intitle}>Bạn có tổng cộng <Text style={{color:'yellow'}}>0</Text> lượt chơi</Text> 
+              <Text style={styles.intitle}>
+                Bạn có tổng cộng <Text style={{ color: "yellow" }}>0</Text> lượt
+                chơi
+              </Text>
             </ImageBackground>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.touchable}>         
+          <TouchableOpacity style={styles.touchable}>
             <ImageBackground
               source={require("../../assets/button2.png")}
               resizeMode="cover"
-              style={styles.imageButton}>             
+              style={styles.imageButton}
+            >
               <Text style={styles.title2}>Quét mã</Text>
             </ImageBackground>
-          </TouchableOpacity>      
-          <TouchableOpacity style={styles.touchable} >         
+          </TouchableOpacity>
+          <TouchableOpacity style={styles.touchable}>
             <ImageBackground
               source={require("../../assets/button2.png")}
               resizeMode="cover"
-              style={styles.imageButton}>             
-              <Text style={styles.title2} >Bộ sưu tập</Text>
+              style={styles.imageButton}
+            >
+              <Text style={styles.title2}>Bộ sưu tập</Text>
             </ImageBackground>
           </TouchableOpacity>
-          <TouchableOpacity style={styles.touchable}>         
+          <TouchableOpacity style={styles.touchable}>
             <ImageBackground
               source={require("../../assets/button2.png")}
               resizeMode="cover"
-              style={styles.imageButton}>             
+              style={styles.imageButton}
+            >
               <Text style={styles.title2}>Chi tiết quà tặng</Text>
             </ImageBackground>
           </TouchableOpacity>
         </View>
       </ImageBackground>
     </View>
-    )
-}
+  );
+};
 
 export default PageMain;
 
 const styles = StyleSheet.create({
-    container: {
+  container: {
     flex: 1,
     justifyContent: "space-between",
   },
@@ -141,7 +212,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
   },
-  
 
   top: {
     flex: 0.4,
@@ -165,11 +235,9 @@ const styles = StyleSheet.create({
     borderRadius: 5,
     padding: 10,
     backgroundColor: "white",
-    
-    
   },
 
-touchable: {
+  touchable: {
     alignItems: "center",
     justifyContent: "center",
     // borderWidth: 5,
@@ -178,21 +246,21 @@ touchable: {
     fontSize: 17,
     textAlign: "center",
     color: "white",
-    fontWeight:'bold',
+    fontWeight: "bold",
   },
   intitle: {
     fontSize: 9,
     textAlign: "center",
     color: "lightgray",
-    marginRight:10,
-    marginTop:4,
+    marginRight: 10,
+    marginTop: 4,
   },
   title2: {
     fontSize: 16,
     textAlign: "center",
     marginBottom: 8,
-    color: '#0063A7',
-    fontWeight:'bold',
+    color: "#0063A7",
+    fontWeight: "bold",
   },
   imageButton: {
     justifyContent: "center",
@@ -206,38 +274,32 @@ touchable: {
     height: 56,
     // borderWidth: 5,
   },
-  fontWhite:{
+  fontWhite: {
     color: "white",
   },
   img: {
-    width: 180,
-    height: 180,
+    width: 208,
+    height: 225,
     // marginLeft: 70,
     marginLeft: 90,
     // marginTop:50,
-    borderWidth: 5,
     
-    },
- 
-  
+  },
 
-
-
-
-centeredView: {
+  centeredView: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',    
+    justifyContent: "center",
+    alignItems: "center",
     marginTop: 22,
   },
   modalView: {
     margin: 20,
-    backgroundColor: '#FBC926',
+    backgroundColor: "#FBC926",
     borderRadius: 20,
     paddingVertical: 10,
     paddingHorizontal: 35,
-    alignItems: 'center',
-    shadowColor: '#000',
+    alignItems: "center",
+    shadowColor: "#000",
     shadowOffset: {
       width: 0,
       height: 2,
@@ -246,31 +308,70 @@ centeredView: {
     shadowOpacity: 0.25,
     shadowRadius: 4,
     elevation: 5,
-   
   },
   button: {
     borderRadius: 10,
     padding: 10,
     width: 110,
     elevation: 2,
-    marginBottom:10,
-    backgroundColor:'#D02027',
-    
-  },  
+    marginBottom: 10,
+    backgroundColor: "#D02027",
+  },
   textStyle: {
-    color: 'white',
-    fontWeight: 'bold',
-    textAlign: 'center',
-    
+    color: "white",
+    fontWeight: "bold",
+    textAlign: "center",
   },
   modalText: {
     marginBottom: 15,
     // borderWidth:1,
     width: 181,
-    color:'#0063A7',
-    fontSize:17,
-     textAlign:'center'
-    
+    color: "#0063A7",
+    fontSize: 17,
+    textAlign: "center",
   },
 
-})
+  modalText2: {
+    marginBottom: 15,
+    // borderWidth:1,
+    width: 195,
+    color: "#D02027",
+    fontSize: 17,
+    textAlign: "center",
+    fontWeight: "bold",
+  },
+  title_Modal: {
+    fontSize: 17,
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+  },
+  intitle_Modal: {
+    fontSize: 11,
+    textAlign: "center",
+    color: "lightgray",
+  },
+  centeredView2: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    marginTop: 22,
+  },
+  modalView2: {
+    margin: 20,
+    backgroundColor: "#FBC926",
+    borderRadius: 15,
+    paddingVertical: 20,
+    paddingHorizontal: 30,
+    alignItems: "center",
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    // borderWidth: 5,
+    shadowOpacity: 0.25,
+    shadowRadius: 4,
+    elevation: 5,
+  },
+});
